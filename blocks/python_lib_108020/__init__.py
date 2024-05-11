aa14306b1dfb7d0365550ff192d46cbd96be1dcc
l1shen <lishen1635@gmail.com>
2024-05-11 14:23:17 +0800

    fix wav tmp path

--- blocks/python_lib_108020/__init__.py
+++ blocks/python_lib_108020/__init__.py
@@ -6,7 +6,7 @@ import uuid
 def main(inputs: dict, context):
   video_file = inputs.get("video_file")
 
-  wav_file_path = os.path.join(tempfile.gettempdir(), "/" + uuid.uuid4().hex, uuid.uuid4().hex + ".wav")
+  wav_file_path = os.path.join(tempfile.gettempdir(), uuid.uuid4().hex, uuid.uuid4().hex + ".wav")
 
   split_audio = subprocess.run(
     ". ~/.x-cmd.root/X; x ffmpeg -i {video_file} -acodec pcm_s16le -ar 16000 -ac 2 {tmp_audio_path}".format(
