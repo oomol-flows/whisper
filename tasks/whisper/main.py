35467fc693404ccb33fb7a8ab4e46966b07ac2e9
Moskize91 <i@taozeyu.com>
2024-12-24 17:45:06 +0800

    docs(project): add annotation & README.md (#2)

--- tasks/whisper/main.py
+++ tasks/whisper/main.py
@@ -4,6 +4,7 @@ from whisper import Whisper
 def main(params: dict):
   model: Whisper = params["model"]
   audio_file: str = params["audio_file"]
+  word_timestamps: bool = params["word_timestamps"]
   prompt: str | None = cast(str, params["prompt"])
   if prompt.strip() == "":
     prompt = None
@@ -11,7 +12,7 @@ def main(params: dict):
   result = model.transcribe(
     audio_file, 
     initial_prompt=prompt,
-    word_timestamps=True,
+    word_timestamps=word_timestamps,
   )
   return { 
     "language": result["language"],
