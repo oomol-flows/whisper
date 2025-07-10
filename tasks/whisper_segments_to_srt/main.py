bed9645246457566a779b0451f17b8be2e5a321d
Moskize91 <i@taozeyu.com>
2025-07-10 15:05:06 +0800

    refactor(tasks): upgrade whisper & apply new formats (#18)

--- tasks/whisper_segments_to_srt/main.py
+++ tasks/whisper_segments_to_srt/main.py
@@ -1,22 +1,30 @@
 import io
 
 
-def main(params: dict):
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+  segments: list[dict]
+class Outputs(typing.TypedDict):
+  srt: str
+#endregion
+
+def main(params: Inputs) -> Outputs:
   segments: list = params["segments"]
   srt_buffer = io.StringIO()
 
   for index, segment in enumerate(segments, start=1):
-    start_time = format_time(segment["start"])
-    end_time = format_time(segment["end"])
+    start_time = _format_time(segment["start"])
+    end_time = _format_time(segment["end"])
     text = segment["text"]
     content = f"{index}\n{start_time} --> {end_time}\n{text}\n\n"
     srt_buffer.write(content)
 
   return { "srt": srt_buffer.getvalue() }
 
-def format_time(seconds):
-    milliseconds = int((seconds % 1) * 1000)
-    seconds = int(seconds)
-    minutes = seconds // 60
-    hours = minutes // 60
-    return f"{hours:02}:{minutes % 60:02}:{seconds % 60:02},{milliseconds:03}"
+def _format_time(seconds):
+  milliseconds = int((seconds % 1) * 1000)
+  seconds = int(seconds)
+  minutes = seconds // 60
+  hours = minutes // 60
+  return f"{hours:02}:{minutes % 60:02}:{seconds % 60:02},{milliseconds:03}"
